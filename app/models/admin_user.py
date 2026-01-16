c76316b79fb271ce5d8c6ef75999a40ec64335ca
mami <mamypoco@gmail.com>
2026-01-16 14:14:36 -0800

    added admin_user + organization

---
+++ app/models/admin_user.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from ..db import db
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from app.models.organization import Organization
+
+class AdminUser(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str]
+    organization_id: Mapped[int] = mapped_column(ForeignKey("organization.id"), unique=True)
+    organization: Mapped["Organization"] = relationship(back_populates="admin_user", uselist=False)
+    created_at: Mapped[datetime]
+
+    @classmethod
+    def from_dict(cls, admin_dict):
+        return cls(
+            username=admin_dict["username"]
+        )
+
+    def to_dict(self):
+        organization_dict = {
+            "organization_id": self.organization_id,
+            "name": self.name,
+            "organization_type": self.organization_type,
+            "website_url": self.website_url,
+            "sites": [site.to_dict() for site in self.sites]
+        }
+        return organization_dict
--- app/models/user.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from ..db import db
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-class User(db.Model):
-  id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-  name: Mapped[str]
-  created_at: Mapped[datetime]
